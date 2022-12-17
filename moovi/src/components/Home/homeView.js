d4a3c83e1b28e32fd36be66c32b3ef885c683d08
Yuanhao <wang0707@vt.edu>
2022-12-17 16:00:25 +0100

    home page modified

--- moovi/src/components/Home/homeView.js
+++ moovi/src/components/Home/homeView.js
@@ -2,6 +2,7 @@ import React from "react";
 import SidebarView from "./sidebarView";
 import MainView from "./mainView";
 import Grid from "@mui/material/Grid";
+import "../../App.css";
 
 // for test
 const catimg = "http://placekitten.com/g/200/300";
@@ -17,7 +18,7 @@ const sidebar = {
 function HomeView() {
   return (
     <div>
-      <Grid container spacing={5} sx={{ mt: 3 }}>
+      <Grid container spacing={5} sx={{ mt: 3 }} className="blur-background">
         <MainView />
         <SidebarView title={sidebar.title} movielist={sidebar.movielist} />
       </Grid>
